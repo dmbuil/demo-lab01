0409bb28c95e6a0336841c4cc7748a0e3eb7151a
Darío MB <dariomartinbuil@outlook.com>
2020-11-10 16:59:46 +0100

    Create create-db.sql

---
+++ create-db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE acmedb;
+USE acmedb; 
+CEATE TABLE `posts` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `title` varchar(255) NOT NULL,
+   `body` varchar(255) NOT NULL,
+   `hostname` varchar(255) NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
