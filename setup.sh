c1ee13f71231226ed8d787d21a18efb13078a8ff
Darío MB <dariomartinbuil@outlook.com>
2020-11-10 17:49:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install apache2 php -y
-sudo apt-get install -y php-{bcmath,bz2,intl,gd,mbstring,mysql,zip} && sudo apt-get install libapache2-mod-php -y
+#sudo apt-get update -y
+#sudo apt-get install apache2 php -y
+#sudo apt-get install -y php-{bcmath,bz2,intl,gd,mbstring,mysql,zip} && sudo apt-get install libapache2-mod-php -y
 
-sudo systemctl enable apache2.service
-sudo systemctl restart apache2.service
+#sudo systemctl enable apache2.service
+#sudo systemctl restart apache2.service
+yum update
+yum install httpd php php-mysql -y
+yum install php-{bcmath,bz2,cli,common,dba,devel,intl,gd,mbstring,mysql,zip} && sudo apt-get install libapache2-mod-php -y
+
+systemctl enable httpd.service
+systemctl restart httpd.service
