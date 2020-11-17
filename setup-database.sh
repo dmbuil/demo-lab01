269a073980f56d16cf8d24ddb26b1843c78a756f
Darío MB <dariomartinbuil@outlook.com>
2020-11-17 20:08:40 +0100

    Creates de DB and the user

--- setup-database.sh
+++ setup-database.sh
@@ -24,25 +24,21 @@ systemctl start mysqld
 
 sleep 5
 
-temppasswd=$(grep "temporary password" /var/log/mysqld.log | awk '{print $13}')
-echo "TMP Pass: $temppasswd" >> /tmp/dploy.log
+MYSQL_ROOT_TMP_PASS=$(grep "temporary password" /var/log/mysqld.log | awk '{print $13}')
+echo "TMP Pass: $MYSQL_ROOT_TMP_PASS" >> /tmp/dploy.log
 
 systemctl restart mysqld
 
-# Runs the Secure installation
-
-MYSQL_ROOT_TMP_PASS=$temppasswd
-MYSQL_ROOT_PASSWORD=$LAB_DATABASE_PASSWD
-
+# Runs the Secure installation using 'expect
 expect -f - <<-EOF
 	set timeout 10
 	spawn mysql_secure_installation
 	expect "Enter password for user root*"
 	send "$MYSQL_ROOT_TMP_PASS\r"
 	expect "New password*"
-	send "$MYSQL_ROOT_PASSWORD\r"
+	send "Telefonica.2020\r"
 	expect "Re-enter new password*"
-	send "$MYSQL_ROOT_PASSWORD\r"
+	send "Telefonica.2020\r"
 	expect "Change the password for root ? ((Press y|Y for Yes, any other key for No)*"
 	send "n\r"
 	expect "Remove anonymous users? (Press y|Y for Yes, any other key for No)*"
@@ -55,3 +51,25 @@ expect -f - <<-EOF
 	send "y\r"
 	expect eof
 EOF
+
+cat << EOF > /root/.my.cnf
+[mysql]
+user="root"
+password="Telefonica.2020"
+[mysqld]
+bind-address = 127.0.0.1    
+bind-address = $LAB_DATABASE_IP
+EOF
+
+# Creates the SQL statetemt to create the user
+cat << EOF > /tmp/dploy/create-user.sql
+CREATE USER '$LAB_DATABASE_USER'@'%' IDENTIFIED BY '$LAB_DATABASE_PASSWD';
+GRANT ALL PRIVILEGES ON * . * TO '$LAB_DATABASE_USER'@'%';
+FLUSH PRIVLEGES;
+EOF
+
+# Creates the Database
+mysql -u < /tmp/dploy/create-db.sql
+
+# Creates the User
+mysql -u < /tmp/dploy/create-user.sql
