0195aa48c4670c7bfb6bda1fd45f2e33e4eab5b8
Darío MB <dariomartinbuil@outlook.com>
2020-11-17 19:42:23 +0100

    changed expect to heredoc

--- setup-database.sh
+++ setup-database.sh
@@ -34,36 +34,24 @@ systemctl restart mysqld
 MYSQL_ROOT_TMP_PASS=$temppasswd
 MYSQL_ROOT_PASSWORD=$LAB_DATABASE_PASSWD
 
-SECURE_MYSQL=$(expect -c "
-
-set timeout 10
-spawn mysql_secure_installation
-
-expect "Enter password for user root:"
-send "$MYSQL_ROOT_TMP_PASS\r"
-
-expect "New password:"
-send "$MYSQL_ROOT_PASSWORD\r"
-
-expect "Re-enter new password:"
-send "$MYSQL_ROOT_PASSWORD\r"
-
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
-send "n\r"
-
-expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Reload privilege tables now?"
-send "y\r"
-
-expect eof
-")
-
-echo "$SECURE_MYSQL"
+expect -f - <<-EOF
+	set timeout 10
+	spawn mysql_secure_installation
+	expect "Enter password for user root*"
+	send "$MYSQL_ROOT_TMP_PASS\r"
+	expect "New password*"
+	send "$MYSQL_ROOT_PASSWORD\r"
+	expect "Re-enter new password*"
+	send "$MYSQL_ROOT_PASSWORD\r"
+	expect "Change the password for root ? ((Press y|Y for Yes, any other key for No)*"
+	send "n\r"
+	expect "Remove anonymous users? (Press y|Y for Yes, any other key for No)*"
+	send "y\r"
+	expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No)*"
+	send "y\r"
+	expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No)*"
+	send "y\r"
+	expect "Reload privilege tables now?*"
+	send "y\r"
+	expect eof
+EOF
