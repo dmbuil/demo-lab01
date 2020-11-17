782762b7dadfbdd27b0d4b286f8a45a7ba9d472b
Darío MB <dariomartinbuil@outlook.com>
2020-11-17 18:10:06 +0100

    added expect package

--- setup-database.sh
+++ setup-database.sh
@@ -5,7 +5,7 @@ yum update -y
 wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -P /tmp/dploy
 rpm -ivh /tmp/dploy/mysql80-community-release-el7-3.noarch.rpm
 
-yum install -y mysql-server
+yum install -y expect mysql-server
 
 LAB_DATABASE_IP=`vmtoolsd --cmd "info-get guestinfo.ovfEnv" | grep 'lab.db.ip' | awk -F\" '{print $4}'`
 LAB_DATABASE_FQDN=`vmtoolsd --cmd "info-get guestinfo.ovfEnv" | grep 'lab.db.fqdn' | awk -F\" '{print $4}'`
@@ -13,11 +13,12 @@ LAB_DATABASE_PASSWD=`vmtoolsd --cmd "info-get guestinfo.ovfEnv" | grep 'lab.db.p
 LAB_DATABASE_NAME=`vmtoolsd --cmd "info-get guestinfo.ovfEnv" | grep 'lab.db.name' | awk -F\" '{print $4}'`
 LAB_DATABASE_USER=`vmtoolsd --cmd "info-get guestinfo.ovfEnv" | grep 'lab.db.user' | awk -F\" '{print $4}'`
 
-echo "$LAB_DATABASE_IP -  $LAB_DATABASE_FQDN - $LAB_DATABASE_PASSWD - $LAB_DATABASE_NAME"
+echo "$LAB_DATABASE_IP -  $LAB_DATABASE_FQDN - $LAB_DATABASE_PASSWD - $LAB_DATABASE_NAME" >> /tmp/dploy.log
 
 systemctl start mysqld
 
 temppasswd=$(grep "temporary password" /var/log/mysqld.log | awk '{print $13}')
-mysqladmin -u root -p $temppasswd password $LAB_DATABASE_PASSWD
+# mysqladmin -u root -p $temppasswd password $LAB_DATABASE_PASSWD
+echo "$temppasswd" >> /tmp/dploy.log
 
 systemctl restart mysqld
